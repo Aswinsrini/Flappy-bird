5f611609630336d672355df2c45db080c4188baa
Aswin <aswinsrinivasan2004@gmail.com>
2024-01-01 00:44:54 +0530

    Game Loop Done...

--- lib/game/flappy_bird_game.dart
+++ lib/game/flappy_bird_game.dart
@@ -1,14 +1,32 @@
-import 'dart:async';
-
+import 'package:flame/components.dart';
+import 'package:flame/events.dart';
 import 'package:flame/game.dart';
 import 'package:flappy_bird/components/background.dart';
 import 'package:flappy_bird/components/bird.dart';
 import 'package:flappy_bird/components/ground.dart';
+import 'package:flappy_bird/components/pipe_group.dart';
+import 'package:flappy_bird/game/configuration.dart';
 
-class FlappyBirdGame extends FlameGame {
+class FlappyBirdGame extends FlameGame with TapDetector, HasCollisionDetection {
+  FlappyBirdGame();
   late Bird bird;
+  Timer interval = Timer(Config.pipeInterval, repeat: true);
   @override
   Future<void> onLoad() async {
-    addAll([Background(), Ground(), bird = Bird()]);
+    addAll(
+      [
+        Background(),
+        Ground(),
+        bird = Bird(),
+        PipeGroup(),
+      ],
+    );
+    interval.onTick = () => add(PipeGroup());
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    interval.update(dt);
   }
 }
